fddd76705d6b2d18f33b6c49c2d7a6ff4d1360de
Dmytro Hniezdilov <wertyzp@gmail.com>
2024-04-12 19:12:35 +0300

    fix

--- src/Types/InputFile.php
+++ src/Types/InputFile.php
@@ -8,11 +8,10 @@ class InputFile extends \CURLFile
 {
     public function __construct(string $filename, string $mimetype = null)
     {
+        if (!file_exists($filename)) {
+            throw new \InvalidArgumentException('File not found');
+        }
         parent::__construct($filename, $mimetype, basename($filename));
     }
 
-    public function getFile(): \CURLFile
-    {
-        return $this->file;
-    }
 }
